a06bab47cd1746b6113dc9798587d4479063bd78
prateekjadwani28 <prateekjadwani.pj@gmail.com>
2024-02-27 01:43:39 +0530

    Task torch backend initial commit

---
+++ src/main/java/com/tasktorch/techend/TaskTorchApplication.java
@@ -0,0 +1,13 @@
+package com.tasktorch.techend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTorchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTorchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tasktorch/techend/TaskTorchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasktorch.techend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTorchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
